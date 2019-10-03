01a866beb53ad7a182a6cfa7146760a5901c97ce
Yuzi Ma <mayuz.bubupu@gmail.com>
2019-10-03 16:36:39 +1300

    feat: toilet sensor

---
+++ index.js
@@ -0,0 +1,68 @@
+const gpio = require('rpi-gpio')
+const logger = require('./logger')
+const fetch = require('node-fetch')
+
+const DOOR_UPDATE_FREQ = 1000 // frequency to check door and update
+const DOOR_FREE = false
+const DOOR_OCCUPIED = true
+
+// let prevMsgTimestamp
+let prevStatus = DOOR_FREE
+
+// config
+const defaultConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '/config.json'), 'utf8'))
+const config = Object.assign({}, defaultConfig, process.env)
+
+const PIN = config.pin
+gpio.setup(PIN, gpio.DIR_IN, readInput)
+
+// Keep checking door every so often
+setInterval(checkStatus, DOOR_UPDATE_FREQ)
+
+// Reads the value of a pin, returning a promise of the result
+function readInput (pin) {
+  return new Promise((resolve, reject) => {
+    gpio.read(pin, function (err, value) {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(value)
+    })
+  })
+}
+
+// check the status of the door and update slack if necessary
+async function checkStatus () {
+  const curStatus = await readDoor()
+  if (curStatus !== prevStatus) {
+    prevStatus = curStatus
+    logger.info('Door changed to ' + curStatus)
+    const msg = {
+      status: curStatus,
+      timestamp: new Date().getTime()
+    }
+    notifyBot(msg)
+    // const msg = (curStatus === DOOR_FREE ? OPEN_MESSAGE : CLOSED_MESSAGE)
+    // update and save the timestamp of the message
+    // const timestamp = await updateSlack(msg, prevMsgTimestamp)
+    // prevMsgTimestamp = timestamp
+  }
+}
+
+// Check if the door is open or closed
+async function readDoor () {
+  const pinValue = await readInput(PIN)
+  logger.info('Read Door Status: ' + pinValue)
+  return pinValue ? DOOR_OCCUPIED : DOOR_FREE
+}
+
+function notifyBot(body) {
+  fetch(`https://young-taiga-63010.herokuapp.com/hubot/toilet/${config.toiletId}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    header: { 'Content-Type': 'application/json' }
+  })
+  .then(res => logger.info(res.json()))
+  .catch(err => logger.error(err))
+}
---
+++ logger.js
@@ -0,0 +1,23 @@
+const winston = require('winston'); // logging
+require('winston-daily-rotate-file');
+const fs = require('fs');
+
+//check log dir exists
+const dir = __dirname + '/log';
+if (!fs.existsSync(dir)){
+  fs.mkdirSync(dir);
+}
+
+const logger = new(winston.Logger)({
+  transports: [
+    new(winston.transports.Console)({ level: 'info'}),
+    new(winston.transports.DailyRotateFile)({
+        level: 'warn',
+        name: 'file',
+        datePattern: '.yyyy-MM-ddTHH',
+        filename: dir + "/toiletbot.log"
+    })
+  ]
+});
+
+module.exports = logger;
